6523528904fbe728823fbc31a5975dc12a66aa7d
Kris Henderson <krishenderson@gmail.com>
2020-02-08 20:05:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,8 +40,11 @@ const max = nodeTree => {
 // }
 }
 
-// {data: 5, left:
-//      {data: 3, left: null, right: null},
-//           right: {data: 7, left: null,
-//              right: {data: 9, left: null, right: null}
+// {data: 5, 
+left: {data: 3, left: null, right: null}, 
+right: {data: 7, left: null, 
+right: {data: 9, left: null, right: null}
+//              
+//      ,
+//          
 //                       }}
