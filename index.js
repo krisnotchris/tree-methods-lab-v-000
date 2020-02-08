5358a9d1f003d481e3f76d8bf6ca8ff057542fdc
Kris Henderson <krishenderson@gmail.com>
2020-02-08 19:39:54 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,42 @@
+function inOrder(currentNode){
+  if(currentNode.left){
+    inOrder(currentNode.left)
+  }
+  console.log(currentNode.data)
+  if(currentNode.right){
+    inOrder(currentNode.right)
+  }
+}
+
+function findOrAdd(rootNode, newNode) {
+  let currentNode = rootNode;
+  if (newNode.data < rootNode.data) {
+    currentNode = rootNode.left;
+    if (currentNode) {
+      findOrAdd(currentNode, newNode);
+    } else {
+      rootNode.left = newNode;
+    }
+  } else if (newNode.data > rootNode.data) {
+    currentNode = rootNode.right;
+    if (currentNode) {
+      findOrAdd(currentNode, newNode);
+    } else {
+      rootNode.right = newNode;
+    }
+  }
+  return true;
+}
+
+const max = node => {
+  let currentNode = node;
+  if (currentNode.right) {
+    return max(currentNode.right)
+  } else {
+    return currentNode.data;
+  }
+}
+
+const min = tree => {
+  return tree;
+}
